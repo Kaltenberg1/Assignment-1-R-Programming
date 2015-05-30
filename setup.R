a7ecb4e85bedb7774b611f37292670c5b937a52b
Kaltenburg1 <daniel.spring@monash.edu>
2015-05-30 11:47:49 +1000

    Updated setup.R
    clears variables in global environment and sets resource path

---
+++ setup.R
@@ -0,0 +1,11 @@
+## This script sets up for a working session. 
+# It initializes all relevant variables.
+# Must clean up global enviroment to avoid the new script using variable values from other scripts
+
+
+## The following line removes all variables from the global environment
+rm(list = ls())
+
+
+## The following line sets up the resource path. This points to the directory for storing input data.
+resource_path <- "~/Documents/Resources/Assignment-1-R-Programming"
